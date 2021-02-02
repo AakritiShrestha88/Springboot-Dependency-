32355b402add5b59f3adc19a87d5792f323c1092
AakritiShresthaShrestha <Aakritibikash.stha88@gmail.com>
2021-02-02 15:59:33 -0500

    Using primary bean

--- src/main/java/com/sfgdi/SfgDiApplication.java
+++ src/main/java/com/sfgdi/SfgDiApplication.java
@@ -17,8 +17,8 @@ public class SfgDiApplication {
 		MyController myController = (MyController) ctx.getBean("myController");
 
 
-		String greeting = myController.syHello();
-		System.out.println(greeting);
+		System.out.println("----------Primary Bean");
+		System.out.println(myController.syHello());
 		System.out.println("------Property");
 		PropertyInjectedController propertyInjectedController =(PropertyInjectedController )ctx.getBean("propertyInjectedController");
 		System.out.println(propertyInjectedController.getGreeting());
--- src/main/java/com/sfgdi/controller/MyController.java
+++ src/main/java/com/sfgdi/controller/MyController.java
@@ -1,11 +1,18 @@
 package com.sfgdi.controller;
 
+import com.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String syHello(){
-        System.out.println("Hello World");
-        return "Hi Folks";
+
+        return greetingService.sayGreeting();
     }
 }
